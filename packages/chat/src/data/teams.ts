42846739a68509efbde20dfdb199cf34896daf0f
eddy zapata <ecz001@live.com>
2024-01-09 14:49:42 -0500

    adding type guards

--- packages/chat/src/data/teams.ts
+++ packages/chat/src/data/teams.ts
@@ -1,11 +1,16 @@
 import { apiCall } from '../utils/networking'
 import { ITeam } from '../types'
+import { isTeam, isTypeArray } from '../type-guards'
 
-let cachedAllTeamsList
+let cachedAllTeamsList: Promise<ITeam[]>  
+
 export async function getAllTeams(): Promise<ITeam[]> {
   if (typeof cachedAllTeamsList === 'undefined')
-    cachedAllTeamsList = apiCall('teams')
-
+    cachedAllTeamsList = apiCall('teams').then((responseData) => {
+  
+      if (isTypeArray(responseData, isTeam)) return responseData
+      else throw new Error('Invalid response for /teams')
+})
   return await cachedAllTeamsList
 }
 
---
+++ packages/chat/src/type-guards.ts
@@ -0,0 +1,24 @@
+import { ITeam } from "./types";
+
+export function isTypeArray<T>(arg: unknown[], guard: (toTest: unknown) => toTest is T): arg is T[] {
+    return Array.isArray(arg) && arg.every(guard)
+}
+
+export function isTeam(arg: unknown): arg is ITeam  {
+    return (
+    typeof arg === 'object'
+    && 'iconUrl' in arg
+    && typeof arg.iconUrl !== 'undefined'
+    
+    && 'name' in arg
+    && typeof arg.name !== 'undefined'
+   
+    && 'id' in arg
+    && typeof arg.id !== 'undefined'
+    
+    && 'channels' in arg
+    && typeof arg.channels !== 'undefined'
+    && Array.isArray(arg.channels)
+    ) 
+    };
+
