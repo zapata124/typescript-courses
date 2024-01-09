2b7a9c72e10fa6c81f825d13482fa637fcb6e1de
eddy zapata <ecz001@live.com>
2024-01-09 14:25:31 -0500

    adding eslint rules

--- packages/chat/.eslintrc.js
+++ packages/chat/.eslintrc.js
@@ -34,6 +34,12 @@ module.exports = {
     'prefer-const': 'error',
   },
   overrides: [
+    { 
+      files: ['src/utils/**/*.{ts,tsx,mts,cts}'],
+      rules: {
+        "@typescript-eslint/strict-boolean-expressions": "error",
+      }
+    },
     {
       /**
        * ESLINT CONFIG
